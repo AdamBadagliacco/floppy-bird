7abfef58048e9a26edf1b6ee91bc0d272381c322
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-07-19 00:02:46 -0500

    Added Pipes and Floor

---
+++ FloppyBird/Assets/Scripts/PipePairScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePairScript : MonoBehaviour
+{
+
+    //Prox and speed will increase with difficulty
+    public float prox; //Proximity
+
+    public float height;
+    public float heightMax; //-0.98
+    public float heightMin; //-5.82
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        height = Random.Range(heightMin, heightMax);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, height, gameObject.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FloppyBird/Assets/Scripts/ScrollLeftScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollLeftScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = new Vector2(speed*-1, 0.0f);
+    }
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x <= -12.42)
+        {
+            gameObject.transform.position = new Vector3(21.12f, -5.2f, -1f);
+        }
+    }
+}
