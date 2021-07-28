ba35b5b986d1b3bfda2c9cd1ba3f10ed6b991c96
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-07-28 12:56:53 -0500

    Added Mario

---
+++ FloppyBird/Assets/FlappyAssets/Scripts/FlappyCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyCoin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        if (collidedObject.transform.tag == "Bird")
+        {
+            collidedObject.GetComponent<BirdScript>().GetCoin();
+            Destroy(gameObject);
+        }
+    }
+}
