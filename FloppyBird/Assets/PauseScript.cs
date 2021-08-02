28211f2767861d2481ebd2094cc6f5d4b675d462
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-08-02 17:34:40 -0500

    #14 Defect Closed

---
+++ FloppyBird/Assets/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public float timeScaleB4Pause;
+    public bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (paused) {
+                paused = false;
+                Time.timeScale = timeScaleB4Pause;
+            }
+            else {
+                timeScaleB4Pause = Time.timeScale;
+                paused = true;
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
