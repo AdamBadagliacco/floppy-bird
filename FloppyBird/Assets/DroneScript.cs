4abb0d1d9996b7c6905f8e15a29eb945d56c3d8b
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-08-02 01:56:35 -0500

    Created Drones, All Pipes Teleport

---
+++ FloppyBird/Assets/DroneScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneScript : MonoBehaviour
+{
+    private float leftSide = 0f;
+    private float rightSide = 199f;
+    private bool goingRight = true;
+    private Rigidbody2D rb;
+
+    //Below attributes are the ones to edit for a different feel/difficulty
+    public float timeRemaining;
+    public float TotalCoolDownTime;
+    public float speed;
+    public GameObject[] spawnableEnemies;
+    public int MaxDroneAmount = 10; 
+
+    public GameObject DroneParent;
+    public GameObject DronePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x <= leftSide) {
+            moveRight();
+        }
+        else if (gameObject.transform.position.x >= rightSide)
+        {
+            moveLeft();
+        }
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            timeRemaining = TotalCoolDownTime;
+            TotalCoolDownTime -= 0.5f;
+            speed = speed + 0.5f;
+            Instantiate(spawnableEnemies[Random.Range(0, spawnableEnemies.Length)], gameObject.transform.position, Quaternion.identity);
+
+            //Resets drone to normal speed and cooldown, and creates another as well 
+            if (TotalCoolDownTime < 1f && DronePrefab.transform.childCount < 10) {
+                GameObject newDrone = Instantiate(DronePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                newDrone.transform.parent = DroneParent.transform;
+                ResetDrone();
+            }
+        }
+    }
+
+    private void moveRight() {
+        rb.velocity = new Vector2(speed, 0.0f);
+        goingRight = true;
+    }
+
+    private void moveLeft() {
+        rb.velocity = new Vector2(-speed, 0.0f);
+        goingRight = false;
+    }
+
+    private void ResetDrone()
+    {
+         TotalCoolDownTime = 4;
+            speed = 5;
+
+    }
+}
